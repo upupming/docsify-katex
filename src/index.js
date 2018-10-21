432d7037554615b66b21d7f4f01db159dd8f61a2
upupming <upupming@gmail.com>
2018-10-21 22:36:18 +0800

    Fixed: docsify render _..._ again as <em> tag

--- src/index.js
+++ src/index.js
@@ -7,8 +7,11 @@ window.docsifyKatex = markdownit({html: true})
 (function () {
   function install(hook) {
     hook.beforeEach(content => {
-      return window.docsifyKatex.render(content);
+      return `<pre>${window.docsifyKatex.render(content)}</pre>`;
     }); 
+    hook.afterEach(function(html, next) {
+      next(html.slice(5, -6))
+    })
   }
 
   $docsify.plugins = [].concat(install, $docsify.plugins);
